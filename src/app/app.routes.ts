20cfa9f52f2f24728a1a35c62ee787b73c22a34c
Fregdy Torrez <fregdy.torrez@superside.com>
2025-06-19 15:16:01 -0600

    feat: adding material lib

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {TaskList} from './components/task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskList },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'atom-challenge-fe';
+  protected title = 'task manager';
 }
