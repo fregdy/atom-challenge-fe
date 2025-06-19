6ea4690ac43150dc9beb39e24ccff81e5fe3f9ab
Fregdy Torrez <fregdy.torrez@superside.com>
2025-06-19 15:17:41 -0600

    feat: adding components

---
+++ src/app/components/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    NgIf
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+}
